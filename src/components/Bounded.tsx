675cd2e3e8c4627f84fd8a77fbef9be0abe3e6da
Aly Ahmed <alynader2004@gmail.com>
2025-08-24 22:29:11 -0400

    fix added

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -7,12 +7,14 @@ type BoundedProps = {
   children: React.ReactNode;
 }
 
-const Bounded = React.forwardRef<HTMLDivElement, BoundedProps>(
+const Bounded = React.forwardRef<HTMLDivElement, React.PropsWithChildren<BoundedProps>>(
   ({ as: Comp = "section", className, children, ...restProps}, ref) => {
     return (
-      <Comp ref={ref}
-      className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
-      {...restProps}>
+      <Comp
+        ref={ref}
+        className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
+        {...restProps}
+      >
         <div className="mx-auto w-full max-w-7xl">
           {children}
         </div>
