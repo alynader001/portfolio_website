00d623e0c066f7e3d6607ee6a2dabbc5c23ce8ac
Aly Ahmed <alynader2004@gmail.com>
2025-08-24 23:15:18 -0400

    Adding Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
@@ -25,6 +26,7 @@ export default function RootLayout({
       >
         <Header/>
         {children}
+        <Analytics />
       </body>
     </html>
   );
