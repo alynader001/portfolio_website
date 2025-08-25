62ef4dffae227d6b5e5db76a4abc2b713c8d9c89
Aly Ahmed <alynader2004@gmail.com>
2025-08-24 22:25:16 -0400

    Typo fixed

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -11,7 +11,7 @@ const Bounded = React.forwardRef<HTMLDivElement, BoundedProps>(
   ({ as: Comp = "section", className, children, ...restProps}, ref) => {
     return (
       <Comp ref={ref}
-      className={clsx("px-4 py-10 md:px md:py-14 lg:py-16", className)}
+      className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
       {...restProps}>
         <div className="mx-auto w-full max-w-7xl">
           {children}
